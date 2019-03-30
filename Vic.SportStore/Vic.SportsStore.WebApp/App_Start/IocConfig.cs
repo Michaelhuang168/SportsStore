4b3ae2bb29192d6aee4c5ffe47ff83cff722b610
Michael Huang <Michael Huang>
2019-03-30 17:47:54 -0400

    Add Moq

--- Vic.SportStore/Vic.SportsStore.WebApp/App_Start/IocConfig.cs
+++ Vic.SportStore/Vic.SportsStore.WebApp/App_Start/IocConfig.cs
@@ -1,10 +1,13 @@
 ﻿using Autofac;
 using Autofac.Integration.Mvc;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vic.SportsStore.Domain.Abstract;
+using Vic.SportsStore.Domain.Emtities;
 
 namespace Vic.SportsStore.WebApp
 {
@@ -15,6 +18,21 @@ namespace Vic.SportsStore.WebApp
             var builder = new ContainerBuilder();
 
             builder.RegisterControllers(typeof(MvcApplication).Assembly).PropertiesAutowired();
+
+
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock
+                .Setup(m => m.Products)
+                .Returns(new List<Product>
+                {
+                new Product { Name = "Football", Price = 25 },
+                new Product { Name = "Surf board", Price = 179 },
+                new Product { Name = "Running shoes", Price = 95 }
+                });
+
+            builder
+                .RegisterInstance<IProductRepository>(mock.Object)
+                .PropertiesAutowired();
 
             var container = builder.Build();
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
