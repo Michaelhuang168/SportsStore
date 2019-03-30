72c057bc8f948889493c168b072d1b72e09e4e94
Michael Huang <Michael Huang>
2019-03-30 17:36:49 -0400

    Add IocConfig.cs

---
+++ Vic.SportStore/Vic.SportsStore.Domain/Abstract/IProductsRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vic.SportsStore.Domain.Emtities;
+
+namespace Vic.SportsStore.Domain.Abstract
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> Products { get; }
+    }
+}
